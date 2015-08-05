85bd24311fd5c2463049ded51fd46d9eb95b133e
csyversen <csyversen@gmail.com>
2015-08-04 21:45:22 -0500

    adding new default route; putting in dummy data for wind forecast

--- app/models/current_wind.rb
+++ app/models/current_wind.rb
@@ -1,2 +1,21 @@
-class CurrentWind < ActiveRecord::Base
+class CurrentWind
+  include ActiveModel::Model
+  require 'forecast_io'
+
+  attr_accessor :lat, :long
+
+  GREENWOOD_BEACH = { lat: '42.043197', long: '-87.671366' }
+
+  def initialize( attributes={} )
+    super
+    ForecastIO.api_key = Rails.application.secrets.forecast_api_key
+  end
+
+  def wind
+    f = ForecastIO.forecast( GREENWOOD_BEACH[:lat], GREENWOOD_BEACH[:long] )
+    f.hourly.data.first.windSpeed
+  end
+
+
+
 end
