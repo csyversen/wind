e06a290ef2a2260d298008341d91e0c5670692f9
Chris Syversen <csyversen@gmail.com>
2015-08-19 11:13:56 -0500

    renaming objects in specs, still in progress

--- spec/helpers/winds_helper_spec.rb
+++ spec/helpers/winds_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
-# the CurrentWindsHelper. For example:
+# the WindsHelper. For example:
 #
-# describe CurrentWindsHelper do
+# describe WindsHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
 #     end
 #   end
 # end
-RSpec.describe CurrentWindsHelper, type: :helper do
+RSpec.describe WindsHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
